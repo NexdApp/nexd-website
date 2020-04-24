157336d9750014fefabf101f40f0b501908240d9
yash296 <wanvari.yash@gmail.com>
2020-04-24 08:07:08 +0530

    added sponsors and partnership

---
+++ src/components/partnership.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-row class="px-10" align="center" justify="center">
+    <v-col cols="12" class="px-6 headline decor" style="color:#4EBF96"
+      >Sponsoren & Partner</v-col
+    >
+    <v-col cols="12">
+      <v-carousel cycle hide-delimiters show-arrows-on-hover height="400">
+        <v-carousel-item v-for="(slide, i) in slides" :key="i">
+          <v-row class="fill-height" align="center" justify="center">
+            <v-col md="3" sm="12" v-for="(icon, j) in slide.icons" :key="j">
+              <v-row align="center" justify="center">
+                <v-img
+                  :src="icon.src"
+                  max-width="200"
+                  max-height="150"
+                  contain
+                ></v-img>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-carousel-item>
+      </v-carousel>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      slides: [
+        {
+          icons: [
+            { src: require("../assets/LogosPartners/IMG0062.png") },
+            { src: require("../assets/LogosPartners/IMG0063.png") },
+            { src: require("../assets/LogosPartners/IMG0064.jpg") },
+          ],
+        },
+        {
+          icons: [
+            { src: require("../assets/LogosPartners/IMG0065.jpg") },
+            { src: require("../assets/LogosPartners/IMG0066.png") },
+            { src: require("../assets/LogosPartners/IMG0067.png") },
+          ],
+        },
+        {
+          icons: [
+            { src: require("../assets/LogosPartners/IMG0068.png") },
+            { src: require("../assets/LogosPartners/IMG0069.png") },
+            { src: require("../assets/LogosPartners/IMG0070.jpg") },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.decor {
+  text-decoration: underline;
+  text-decoration-style: solid;
+  text-decoration-color: #0c2e451c;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,12 @@ module.exports = {
   transpileDependencies: ["vuetify"],
   // publicPath: process.env.NODE_ENV === "production" ? "/nexd-website/" : "/"
   // root directory helpful for production deployment on netlify
-  publicPath: "/"
+  publicPath: "/",
+  chainWebpack: (config) => {
+    config.module
+      .rule("images")
+      .use("url-loader")
+      .loader("url-loader")
+      .tap((options) => Object.assign(options, { limit: 4194304 }));
+  },
 };
