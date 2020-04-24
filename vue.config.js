c497d36805ea795a73091426e852cef46204f9bc
yash296 <wanvari.yash@gmail.com>
2020-04-24 08:16:35 +0530

    code cleaned

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,4 @@ module.exports = {
   // publicPath: process.env.NODE_ENV === "production" ? "/nexd-website/" : "/"
   // root directory helpful for production deployment on netlify
   publicPath: "/",
-  chainWebpack: (config) => {
-    config.module
-      .rule("images")
-      .use("url-loader")
-      .loader("url-loader")
-      .tap((options) => Object.assign(options, { limit: 4194304 }));
-  },
 };
